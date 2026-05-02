ba4ae122ccad1748b9b0951bbd50dd794238b80a
xuyujing611-a11y <xuyujing611@gmail.com>
2026-05-02 12:07:38 +0800

    [FIX] B49 AI气泡位置移至头像左下方

--- src/client/js/game.js
+++ src/client/js/game.js
@@ -2136,14 +2136,14 @@ GameScene.prototype._showPlayBubble = function (aiId, event, context) {
     self.playBubbleElements = [];
 
     var isDuidui = (aiId === 'duidui');
-    var y = isDuidui ? 120 : 165;
+    var y = 55;
     var aiDisplayName = isDuidui ? '王怼怼' : '苏甜甜';
     var avatarX = isDuidui ? 80 : 880;
     var avatarColor = isDuidui ? 0x4FC3F7 : 0xFFB74D;
 
     var bubbleW = Math.min(280, 140 + line.length * 10);
     var bubbleH = 36;
-    var bubbleX = isDuidui ? 230 : (avatarX - bubbleW - 30);
+    var bubbleX = isDuidui ? 90 : 690;
     var bubbleY = y + 10;
     var cornerRadius = isDuidui ? 12 : 4;
     var bubbleBgColor = isDuidui ? 0x1B5E20 : 0x311B92;
